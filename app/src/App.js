f3f36b404a685ec83dd649fab0ef9fd0858f3929
PauloFurtunatoAlexandre <furtuna.alexandre@gmail.com>
2020-07-29 16:11:20 -0500

    added the fetching data for animes

--- app/src/App.js
+++ app/src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { connect } from "react-redux";
+import "./App.css";
 
-function App() {
+import AnimeSearch from "./components/animes-search/animes-search";
+import AnimesList from "./components/animes-list/animes-list";
+
+function App(props) {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Ghibli Anime Studio Search</h2>
+      <AnimeSearch />
+      <AnimesList />
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    animes: state.animes,
+  };
+};
+
+export default connect(mapStateToProps)(App);
